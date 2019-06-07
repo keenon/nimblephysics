f47bd97be782b5c01dda8866280bb0983b23cec0
Jeongseok Lee <jslee02@gmail.com>
2019-06-07 03:41:03 -0700

    Use ninja instead of make

--- .ci/script.sh
+++ .ci/script.sh
@@ -74,6 +74,7 @@ if [ "$OS_NAME" = "linux" ]; then
 fi
 
 cmake .. \
+  -Gninja \
   -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
   -DDART_BUILD_DARTPY=$BUILD_DARTPY \
   -DDART_VERBOSE=ON \
@@ -119,6 +120,6 @@ else
   # Build an example using installed DART
   cd $BUILD_DIR/examples/hello_world
   mkdir build && cd build
-  cmake ..
+  cmake -Gninja ..
   make -j$num_threads
 fi
