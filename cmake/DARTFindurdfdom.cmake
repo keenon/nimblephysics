7cb9a9496520b7500a1d96227b5b5ec6bca1b716
Jeongseok Lee <jslee02@gmail.com>
2019-07-07 01:43:49 -0700

    Try to fix not finding urdf include dirs

--- cmake/DARTFindurdfdom.cmake
+++ cmake/DARTFindurdfdom.cmake
@@ -8,11 +8,18 @@
 
 find_package(urdfdom QUIET CONFIG)
 
-message(STATUS "[DEBUG] CMAKE_INSTALL_PREFIX: ${CMAKE_INSTALL_PREFIX}")
-message(STATUS "[DEBUG] urdfdom_INCLUDE_DIRS: ${urdfdom_INCLUDE_DIRS}")
-message(STATUS "[DEBUG] TinyXML_INCLUDE_DIRS: ${TinyXML_INCLUDE_DIRS}")
-message(STATUS "[DEBUG] urdfdom_headers_INCLUDE_DIRS: ${urdfdom_headers_INCLUDE_DIRS}")
-message(STATUS "[DEBUG] console_bridge_INCLUDE_DIRS: ${console_bridge_INCLUDE_DIRS}")
+if(MSVC)
+  list(REMOVE_ITEM urdfdom_INCLUDE_DIRS "/include")
+
+  find_package(tinyxml REQUIRED)
+  list(APPEND urdfdom_INCLUDE_DIRS ${tinyxml_INCLUDE_DIRS})
+
+  message(STATUS "[DEBUG] CMAKE_INSTALL_PREFIX: ${CMAKE_INSTALL_PREFIX}")
+  message(STATUS "[DEBUG] urdfdom_INCLUDE_DIRS: ${urdfdom_INCLUDE_DIRS}")
+  message(STATUS "[DEBUG] TinyXML_INCLUDE_DIRS: ${TinyXML_INCLUDE_DIRS}")
+  message(STATUS "[DEBUG] urdfdom_headers_INCLUDE_DIRS: ${urdfdom_headers_INCLUDE_DIRS}")
+  message(STATUS "[DEBUG] console_bridge_INCLUDE_DIRS: ${console_bridge_INCLUDE_DIRS}")
+endif()
 
 if(urdfdom_FOUND AND NOT TARGET urdfdom)
   add_library(urdfdom INTERFACE IMPORTED)
