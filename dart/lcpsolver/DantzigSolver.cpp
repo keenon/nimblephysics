d6e98a4ca1c2716ba63596ee22ae4214a6d35e72
Jeongseok Lee <jeoslee@amazon.com>
2020-01-03 19:08:25 -0800

    Implementing primitive LCP operations

--- dart/lcpsolver/DantzigSolver.cpp
+++ dart/lcpsolver/DantzigSolver.cpp
@@ -42,6 +42,191 @@ namespace dart {
 namespace lcpsolver {
 
 //==============================================================================
+void solve_l1(const double* L, double* B, int n, int lskip1)
+{
+  // Declare variables - Z matrix, p and q vectors, etc
+  double Z11;
+  double m11;
+  double Z21;
+  double m21;
+  double p1;
+  double q1;
+  double p2;
+  double* ex;
+  const double* ell;
+  int i;
+  int j;
+
+  // Compute all 2 x 1 blocks of X
+  for (i = 0; i < n; i += 2)
+  {
+    // Compute all 2 x 1 block of X, from rows i..i+2-1
+
+    // Set the Z matrix to 0
+    Z11 = 0;
+    Z21 = 0;
+    ell = L + i * lskip1;
+    ex = B;
+
+    // The inner loop that computes outer products and adds them to Z
+    for (j = i - 2; j >= 0; j -= 2)
+    {
+      p1 = ell[0];
+      q1 = ex[0];
+      m11 = p1 * q1;
+      p2 = ell[lskip1];
+      m21 = p2 * q1;
+      Z11 += m11;
+      Z21 += m21;
+
+      // Compute outer product and add it to the Z matrix
+      p1 = ell[1];
+      q1 = ex[1];
+      m11 = p1 * q1;
+      p2 = ell[1 + lskip1];
+      m21 = p2 * q1;
+
+      // Advance pointers
+      ell += 2;
+      ex += 2;
+      Z11 += m11;
+      Z21 += m21;
+    }
+
+    // Compute left-over iterations
+    j += 2;
+    for (; j > 0; j--)
+    {
+      // Compute outer product and add it to the Z matrix
+      p1 = ell[0];
+      q1 = ex[0];
+      m11 = p1 * q1;
+      p2 = ell[lskip1];
+      m21 = p2 * q1;
+
+      // Advance pointers
+      ell += 1;
+      ex += 1;
+      Z11 += m11;
+      Z21 += m21;
+    }
+
+    // Finish computing the X(i) block
+    Z11 = ex[0] - Z11;
+    ex[0] = Z11;
+    p1 = ell[lskip1];
+    Z21 = ex[1] - Z21 - p1 * Z11;
+    ex[1] = Z21;
+  }
+}
+
+//==============================================================================
+DantzigLcp::DantzigLcp(Eigen::MatrixXd& A, Eigen::VectorXd& b)
+  : mA(A), mb(b), mn(static_cast<int>(A.rows()))
+{
+  // Do nothing (yet)
+}
+
+//==============================================================================
+void DantzigLcp::swapRowsAndCols(int index1, int index2)
+{
+  mA.col(index1).swap(mA.col(index2));
+  mA.row(index1).swap(mA.row(index2));
+}
+
+//==============================================================================
+void DantzigLcp::transfer_i_to_C(int i)
+{
+  if (mNumC > 0)
+  {
+    {
+      // Do something
+    }
+
+    mLdlt.D[mNumC] = 1.0 / (mA(i, i) - mell.head(mNumC).dot(mDell.head(mNumC)));
+  }
+  else
+  {
+    mLdlt.D[0] = 1.0 / mA(i, i);
+  }
+}
+
+//==============================================================================
+void DantzigLcp::transfer_i_to_N(int /*i*/)
+{
+  mNumN++;
+}
+
+//==============================================================================
+DantzigLcpOptimized::DantzigLcpOptimized(Eigen::MatrixXd& A, Eigen::VectorXd& b)
+  : mA(A), mb(b), mn(static_cast<int>(A.rows()))
+{
+  mARows.resize(static_cast<size_t>(mn));
+  double* ptr = mA.data();
+  for (auto i = 0; i < mn; ++i)
+  {
+    mARows[static_cast<size_t>(i)] = ptr;
+    ptr += mn;
+  }
+}
+
+//==============================================================================
+int DantzigLcpOptimized::indexC(int i) const
+{
+  return i;
+}
+
+//==============================================================================
+int DantzigLcpOptimized::indexN(int i) const
+{
+  return i + mNumC;
+}
+
+//==============================================================================
+double DantzigLcpOptimized::Aii(size_t i) const
+{
+  return mARows[i][i];
+}
+
+//==============================================================================
+double DantzigLcpOptimized::AiC_times_qC(
+    size_t i, const Eigen::VectorXd& x) const
+{
+  const auto& AC = mA.row(mRowIndex[static_cast<int>(i)]).head(mNumC);
+  const auto& xC = x.head(mNumC);
+  return AC.dot(xC);
+}
+
+//==============================================================================
+double DantzigLcpOptimized::AiN_times_qN(
+    size_t i, const Eigen::VectorXd& x) const
+{
+  const auto& AN = mA.row(mRowIndex[static_cast<int>(i)]).segment(mNumC, mNumN);
+  const auto& xN = x.segment(mNumC, mNumN);
+  return AN.dot(xN);
+}
+
+//==============================================================================
+void DantzigLcpOptimized::ANC_times_qC(
+    Eigen::VectorXd& p, const Eigen::VectorXd& q)
+{
+  for (auto i = 0; i < mNumN; ++i)
+  {
+    p[mNumC + i] = mA.row(mRowIndex[mNumC + static_cast<int>(i)])
+                       .head(mNumC)
+                       .dot(q.head(mNumC));
+  }
+}
+
+//==============================================================================
+void DantzigLcpOptimized::solve1(double* a, int i, int dir, int only_transfer)
+{
+  if (mNumC > 0)
+  {
+  }
+}
+
+//==============================================================================
 DantzigSolver::DantzigSolver()
 {
   // Do nothing
@@ -54,10 +239,18 @@ DantzigSolver::~DantzigSolver()
 }
 
 //==============================================================================
+void compute_f_direction(
+    int n, int index, const Eigen::MatrixXd& A, Eigen::VectorXd& df)
+{
+  df.setZero(n);
+  df[index] = 1;
+}
+
+//==============================================================================
 bool DantzigSolver::solve(
     const Eigen::MatrixXd& A,
     const Eigen::VectorXd& b,
-    Eigen::VectorXd* x,
+    Eigen::VectorXd& x,
     int numContacts,
     double mu,
     int numDir)
@@ -67,26 +260,34 @@ bool DantzigSolver::solve(
 
   const auto n = A.rows();
 
-  x->resize(n);
-  Eigen::VectorXd w = Eigen::VectorXd::Zero(n);
-  Eigen::VectorXd lo = Eigen::VectorXd::Zero(n);
-  Eigen::VectorXd hi = Eigen::VectorXd::Constant(n, math::constantsd::inf());
-  Eigen::VectorXi fIndex = Eigen::VectorXi::Constant(n, -1);
+  x.setZero(n);
+  Eigen::VectorXd w = b;
 
-  for (int i = 0; i < numContacts; ++i)
+  Eigen::VectorXi type = Eigen::VectorXi::Zero(0);
+
+  for (auto i = 0; i < n; ++i)
   {
-    const auto index1 = numContacts + i * 2 + 0;
-    const auto index2 = numContacts + i * 2 + 0;
-
-    fIndex[index1] = i;
-    fIndex[index2] = i;
-
-    lo[index1] = -mu;
-    lo[index2] = -mu;
-
-    hi[index1] = mu;
-    hi[index2] = mu;
   }
+
+  //  Eigen::VectorXd lo = Eigen::VectorXd::Zero(n);
+  //  Eigen::VectorXd hi = Eigen::VectorXd::Constant(n,
+  //  math::constantsd::inf()); Eigen::VectorXi fIndex =
+  //  Eigen::VectorXi::Constant(n, -1);
+
+  //  for (int i = 0; i < numContacts; ++i)
+  //  {
+  //    const auto index1 = numContacts + i * 2 + 0;
+  //    const auto index2 = numContacts + i * 2 + 0;
+
+  //    fIndex[index1] = i;
+  //    fIndex[index2] = i;
+
+  //    lo[index1] = -mu;
+  //    lo[index2] = -mu;
+
+  //    hi[index1] = mu;
+  //    hi[index2] = mu;
+  //  }
 
   return true;
 }
--- dart/lcpsolver/DantzigSolver.hpp
+++ dart/lcpsolver/DantzigSolver.hpp
@@ -33,10 +33,90 @@
 #ifndef DART_LCPSOLVER_DANTZIGSOLVER_HPP_
 #define DART_LCPSOLVER_DANTZIGSOLVER_HPP_
 
+#include <memory>
+#include <vector>
+
 #include <Eigen/Dense>
 
 namespace dart {
 namespace lcpsolver {
+
+// Solves L*X=B, with B containing 1 right hand sides where:
+//   L is an n*n lower triangular matrix with ones on the diagonal.
+//   L is stored by rows and its leading dimension is lskip.
+//   B is an n*1 matrix that contains the right hand sides.
+//   B is stored by columns and its leading dimension is also lskip.
+//   B is overwritten with X.
+// This processes blocks of 2*2.
+// if this is in the factorizer source file, n must be a multiple of 2.
+void solve_l1(const double* L, double* B, int n, int lskip1);
+
+struct LdltFactorization
+{
+  Eigen::MatrixXd L;
+  Eigen::VectorXd D;
+};
+
+class DantzigLcp
+{
+public:
+  DantzigLcp(Eigen::MatrixXd& A, Eigen::VectorXd& b);
+
+private:
+  void swapRowsAndCols(int index1, int index2);
+  void transfer_i_to_C(int i);
+  void transfer_i_to_N(int i);
+
+  Eigen::MatrixXd& mA;
+  Eigen::VectorXd& mb;
+
+  const int mn;
+  int mNumC{0};
+  int mNumN{0};
+
+  Eigen::VectorXd mDell;
+  Eigen::VectorXd mell;
+  Eigen::VectorXd mtmp;
+
+  LdltFactorization mLdlt;
+};
+
+class DantzigLcpOptimized
+{
+public:
+  DantzigLcpOptimized(Eigen::MatrixXd& A, Eigen::VectorXd& b);
+
+private:
+  void swapRowsAndCols(int index1, int index2);
+  void transfer_i_to_C(int i);
+  void transfer_i_to_N(int i);
+
+  int indexC(int i) const;
+  int indexN(int i) const;
+  double Aii(size_t i) const;
+  double AiC_times_qC(size_t i, const Eigen::VectorXd& x) const;
+  double AiN_times_qN(size_t i, const Eigen::VectorXd& x) const;
+  void ANC_times_qC(Eigen::VectorXd& p, const Eigen::VectorXd& q);
+  void solve1(double* a, int i, int dir, int only_transfer);
+
+  Eigen::MatrixXd& mA;
+  Eigen::VectorXd& mb;
+
+  const int mn;
+  int mNumC{0};
+  int mNumN{0};
+
+  Eigen::VectorXd mDell;
+  Eigen::VectorXd mell;
+  Eigen::VectorXd mtmp;
+
+  LdltFactorization mLdlt;
+
+  std::vector<double*> mARows;
+  Eigen::VectorXi mRowIndex;
+
+  int* mC;
+};
 
 class DantzigSolver
 {
@@ -48,7 +128,7 @@ public:
   static bool solve(
       const Eigen::MatrixXd& A,
       const Eigen::VectorXd& b,
-      Eigen::VectorXd* x,
+      Eigen::VectorXd& x,
       int numContacts = 0,
       double mu = 0,
       int numDir = 4);
