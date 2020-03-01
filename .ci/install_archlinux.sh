6c80399d440264b4f07b087d4e181a3832ae9a16
Jeongseok Lee <jslee02@gmail.com>
2020-03-01 07:16:04 -0800

    Test

--- .ci/install_archlinux.sh
+++ .ci/install_archlinux.sh
@@ -17,10 +17,8 @@ if [ -z "$COMPILER" ]; then
   COMPILER=gcc
 fi
 
-whoami
-
 # Build tools
-yay -S cmake
+yay -Syu cmake
 
 # Required dependencies
 yay -S assimp boost eigen fcl freeglut libccd libgl
