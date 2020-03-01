c6ba21dc161d5ff99de8f339d7ff94c5198af121
Jeongseok Lee <jslee02@gmail.com>
2020-02-29 23:09:28 -0800

    Use lsb-release to detect linux distro

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,11 +2,11 @@
 set -ex
 
 if [ "${OSTYPE//[0-9.]/}" == "linux-gnu" ]; then
-  # if [ $(lsb_release -si) = "Ubuntu" ]; then
-  #   '.ci/install_ubuntu.sh'
-  # elif [ $(lsb_release -si) = "Arch" ]; then
-  #   '.ci/install_archlinux.sh'
-  # fi
+  if [ $(lsb_release -si) = "Ubuntu" ]; then
+    '.ci/install_ubuntu.sh'
+  elif [ $(lsb_release -si) = "Arch" ]; then
+    '.ci/install_archlinux.sh'
+  fi
 elif [ "${OSTYPE//[0-9.]/}" == "darwin"  ]; then
   '.ci/install_osx.sh'
 fi
