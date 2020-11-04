9ca7bdd0fdbd0dbcb00840a695beb1adaf67eb75
Keenon Werling <keenonwerling@gmail.com>
2020-11-03 18:18:07 -0800

    Attempting to fix linux CI builds

--- ci/manylinux/3.7/build_wheel.sh
+++ ci/manylinux/3.7/build_wheel.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-docker build .
+VERSION=$(cat ../../../VERSION.txt)
+docker build . --build-arg VERSION=${VERSION}
 IMAGE_ID=$(docker images | awk '{print $3}' | awk 'NR==2')
 echo "Build image ID $IMAGE_ID"
 CONTAINER_ID=$(docker create $IMAGE_ID)
 echo "Started container ID $CONTAINER_ID"
-VERSION=$(cat ../../../VERSION.txt)
 mkdir -p ../../../wheelhouse
 docker cp $CONTAINER_ID:/wheelhouse/diffdart-${VERSION}-cp37-cp37m-manylinux2010_x86_64.whl ../../../wheelhouse
 docker rm -v $CONTAINER_ID
