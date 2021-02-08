7f5e03486895cc31de76d66a996043a3a889302d
Keenon Werling <keenonwerling@gmail.com>
2021-02-08 10:26:19 -0800

    Trying another approach to breaking CI race condition

--- ci/manylinux/3.6/build_wheel.sh
+++ ci/manylinux/3.6/build_wheel.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-pushd ../../..
-VERSION=$(cat ./VERSION.txt)
-docker build . -f ci/manylinux/3.6/Dockerfile --build-arg VERSION=${VERSION}
+VERSION=$(cat ../../../VERSION.txt)
+GIT_HASH=$(git log --pretty=format:'%H' -n 1)
+docker build . --build-arg VERSION=${VERSION} --build-arg GIT_HASH=${GIT_HASH}
 IMAGE_ID=$(docker images | awk '{print $3}' | awk 'NR==2')
 echo "Build image ID $IMAGE_ID"
 CONTAINER_ID=$(docker create $IMAGE_ID)
 echo "Started container ID $CONTAINER_ID"
-mkdir -p ./wheelhouse
-docker cp $CONTAINER_ID:/wheelhouse/diffdart-${VERSION}-cp36-cp36m-manylinux2014_x86_64.whl ./wheelhouse
+mkdir -p ../../../wheelhouse
+docker cp $CONTAINER_ID:/wheelhouse/diffdart-${VERSION}-cp36-cp36m-manylinux2014_x86_64.whl ../../../wheelhouse
 docker rm -v $CONTAINER_ID
-popd
