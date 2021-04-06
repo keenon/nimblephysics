6502f7f8ec0d194fdb743e8981434b684227d271
Keenon Werling <keenon@stanford.edu>
2021-04-05 20:21:52 -0700

    Fixing Mac install dependencies

--- ci/mac/install_dependencies.sh
+++ ci/mac/install_dependencies.sh
@@ -225,7 +225,6 @@ pushd mpfr-4.1.0
 ./configure
 make
 sudo make install
-sudo ldconfig
 popd
 rm -rf mpfr-4.1.0
 rm mpfr-4.1.0.tar.gz
@@ -235,10 +234,9 @@ brew install yasm m4
 curl http://mpir.org/mpir-3.0.0.tar.bz2 > mpir-3.0.0.tar.bz2
 tar -xf mpir-3.0.0.tar.bz2
 pushd mpir-3.0.0
-./configure --enable-cxx
+CC=gcc ./configure --enable-cxx
 make
 sudo make install
-sudo ldconfig
 popd
 rm -rf mpir-3.0.0
 rm mpir-3.0.0.tar.bz2
@@ -247,7 +245,7 @@ rm mpir-3.0.0.tar.bz2
 wget https://github.com/advanpix/mpreal/archive/refs/tags/mpfrc++-3.6.8.tar.gz
 tar -xzf mpfrc++-3.6.8.tar.gz
 pushd mpreal-mpfrc-3.6.8
-sudo cp mpreal.h /usr/include/
+sudo cp mpreal.h /usr/local/include/
 popd
 rm -rf mpreal-mpfrc-3.6.8
 rm mpfrc++-3.6.8.tar.gz
