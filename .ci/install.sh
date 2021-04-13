93cf943d58035915a6dfb23f679d3aff0982f110
Keenon Werling <keenonwerling@gmail.com>
2021-04-12 19:51:03 -0700

    Cleaning out folders we don't use in this fork

--- .ci/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-if [ "$OS_NAME" = "linux" ]; then '.ci/install_linux.sh' ; fi
-if [ "$OS_NAME" = "osx"   ]; then '.ci/install_osx.sh'   ; fi
--- .ci/install_osx.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-brew update > /dev/null
-brew bundle
-
-pip3 install -U pytest
