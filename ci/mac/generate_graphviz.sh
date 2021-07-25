d948bc0c68972465fe591fa648e8fd5f54df5c1c
Keenon Werling <keenon@stanford.edu>
2021-07-24 18:30:59 -0700

    OSX CI: Fixing Python < 3.8 on generate_graphviz.sh

--- ci/mac/generate_graphviz.sh
+++ ci/mac/generate_graphviz.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -e
 
+# Find our python paths
+export PYTHON_INCLUDE=$(python3-config --includes)
+echo "PYTHON_INCLUDE=${PYTHON_INCLUDE}"
+export PYTHON_LIB=$(python3-config --libs)
+echo "PYTHON_LIB=${PYTHON_LIB}"
+
 pushd ../..
+
+mkdir -p bin
+ln -sfn $(which python3) ./bin/python
+export PATH=$(pwd)/bin:${PATH}
+echo "python=$(which python)"
+
 mkdir build_viz
 pushd build_viz
 cmake .. --graphviz=viz.dot
