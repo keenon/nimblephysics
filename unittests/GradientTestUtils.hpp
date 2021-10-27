2519f43490f3eceae68038d4d6189f2077e82f1f
Ericcsr <ericcsr@connect.hku.hk>
2021-10-27 11:37:30 +0000

    Fix merge bug in testGradientUtils

--- unittests/GradientTestUtils.hpp
+++ unittests/GradientTestUtils.hpp
@@ -524,9 +524,7 @@ bool verifyClassicProjectionIntoClampsMatrix(
     "
         << std::endl
         << comparison << std::endl;
-
     // Show the contact accels
-
     Eigen::VectorXs realContactAccels
         = classicPtr->mGradientMatrices[0]->mA * contactConstraintForces;
     Eigen::VectorXs analyticalContactAccels
@@ -571,7 +569,6 @@ bool verifyClassicProjectionIntoClampsMatrix(
     accelComparison.col(1) = analyticalAccel;
     std::cout << "Real accel - Analytical accel: " << std::endl
               << accelComparison << std::endl;
-
     std::cout << "Analytical error (should be zero):" << std::endl
               << analyticalError << std::endl;
     */
@@ -1402,7 +1399,6 @@ bool verifyF_c(WorldPtr world)
 
     /*
     // Suspicious inputs to check if dB is broken:
-
     Eigen::MatrixXs dMinv_f = getJacobianOfMinv(world, f, wrt);
     Eigen::MatrixXs dA_c_f
         = getJacobianOfClampingConstraintsTranspose(world, v_f);
@@ -1456,7 +1452,6 @@ bool verifyF_c(WorldPtr world)
       return false;
     }
 
-    
     Eigen::MatrixXs bruteForceEstimatedJac
         = classicPtr->finiteDifferenceJacobianOfEstimatedConstraintForce(
             world, WithRespectTo::POSITION);
@@ -1529,7 +1524,7 @@ bool verifyF_c(WorldPtr world)
       }
       return false;
     }
-    
+
     bruteForceJacB
         = classicPtr->finiteDifferenceJacobianOfLCPOffsetClampingSubset(
             world, WithRespectTo::VELOCITY);
@@ -1574,7 +1569,6 @@ bool verifyF_c(WorldPtr world)
       return false;
     }
 
-    
     bruteForceEstimatedJac
         = classicPtr->finiteDifferenceJacobianOfEstimatedConstraintForce(
             world, WithRespectTo::VELOCITY);
@@ -1626,7 +1620,7 @@ bool verifyF_c(WorldPtr world)
       }
       return false;
     }
-    
+
     bruteForceJacB
         = classicPtr->finiteDifferenceJacobianOfLCPOffsetClampingSubset(
             world, WithRespectTo::FORCE);
@@ -1670,7 +1664,7 @@ bool verifyF_c(WorldPtr world)
                 << std::endl;
       return false;
     }
-    
+
     bruteForceEstimatedJac
         = classicPtr->finiteDifferenceJacobianOfEstimatedConstraintForce(
             world, WithRespectTo::FORCE);
@@ -1722,7 +1716,6 @@ bool verifyF_c(WorldPtr world)
       }
       return false;
     }
-    
   }
 
   snapshot.restore();
@@ -1815,11 +1808,13 @@ VelocityTest runVelocityTest(WorldPtr world)
   Eigen::MatrixXs A_c_ub_E = A_c + A_ub * E;
   Eigen::VectorXs tau = world->getControlForces();
   Eigen::MatrixXs damping = classicPtr->getDampingVector(world).asDiagonal();
-  Eigen::MatrixXs spring_stiffs = classicPtr->getSpringStiffVector(world).asDiagonal();
+  Eigen::MatrixXs spring_stiffs
+      = classicPtr->getSpringStiffVector(world).asDiagonal();
   Eigen::VectorXs p_rest = classicPtr->getRestPositions(world);
   s_t dt = world->getTimeStep();
-  Eigen::VectorXs damping_force = damping*preStepVelocity;
-  Eigen::VectorXs spring_force = spring_stiffs*(preStepPosition - p_rest + preStepVelocity*dt);
+  Eigen::VectorXs damping_force = damping * preStepVelocity;
+  Eigen::VectorXs spring_force
+      = spring_stiffs * (preStepPosition - p_rest + preStepVelocity * dt);
 
   Eigen::MatrixXs Minv = world->getInvMassMatrix();
   Eigen::VectorXs C = world->getCoriolisAndGravityAndExternalForces();
@@ -1854,7 +1849,8 @@ VelocityTest runVelocityTest(WorldPtr world)
 
   Eigen::VectorXs realImpulses = classicPtr->getClampingConstraintImpulses();
 
-  Eigen::VectorXs preSolveV = preStepVelocity + dt * Minv * (tau - C - damping_force - spring_force);
+  Eigen::VectorXs preSolveV
+      = preStepVelocity + dt * Minv * (tau - C - damping_force - spring_force);
 
   Eigen::VectorXs f_cDeltaV;
   if (A_c.cols() == 0)
@@ -2188,9 +2184,11 @@ bool verifyFeatherstoneJacobians(WorldPtr world)
     Eigen::VectorXs x = Eigen::VectorXs::Random(skel->getNumDofs());
     for (int j = 0; j < skel->getNumBodyNodes(); j++)
     {
-      bool success = skel->getBodyNode(j)->debugJacobianOfMForward(WithRespectTo::POSITION, x);
+      bool success = skel->getBodyNode(j)->debugJacobianOfMForward(
+          WithRespectTo::POSITION, x);
       EXPECT_TRUE(success);
-      if (!success) return false;
+      if (!success)
+        return false;
     }
     Eigen::MatrixXs MinvX
         = skel->finiteDifferenceJacobianOfM(x, WithRespectTo::POSITION);
@@ -2199,7 +2197,8 @@ bool verifyFeatherstoneJacobians(WorldPtr world)
       bool success = skel->getBodyNode(j)->debugJacobianOfMBackward(
           WithRespectTo::POSITION, x, MinvX);
       EXPECT_TRUE(success);
-      if (!success) return false;
+      if (!success)
+        return false;
     }
   }
   return true;
@@ -2377,13 +2376,11 @@ bool verifyVelGradients(WorldPtr world, VectorXs worldVel)
       // && verifyJacobianOfProjectionIntoClampsMatrix(world, worldVel,
       // POSITION)
       && verifyRecoveredLCPConstraints(world, worldVel)
-      && verifyPerturbedF_c(world) 
-      && verifyF_c(world)
+      && verifyPerturbedF_c(world) && verifyF_c(world)
       && verifyForceVelJacobian(world, worldVel)
       && verifyVelVelJacobian(world, worldVel)
       && verifyFeatherstoneJacobians(world)
-      && verifyPosVelJacobian(world, worldVel) 
-      && verifyNextV(world));
+      && verifyPosVelJacobian(world, worldVel) && verifyNextV(world));
 }
 
 bool verifyPosPosJacobianApproximation(
@@ -6108,7 +6105,6 @@ bool verifyPerturbedScrewAxisForPosition(WorldPtr world)
                 axis, wrt, -EPS);
         Eigen::Vector6s bruteForceNeg
             = constraints[q]->bruteForceScrewAxisForPosition(axis, wrt, -EPS);
-
         Eigen::Vector6s finiteDifferenceGradient
             = (bruteForce - bruteForceNeg) / (2 * EPS);
         Eigen::Vector6s finiteDifferenceAnalyticalGradient
@@ -6692,21 +6688,34 @@ bool verifyIKPositionJacobians(WorldPtr world)
     for (int j = 0; j < skel->getNumBodyNodes(); j++)
     {
       BodyNode* body = skel->getBodyNode(j);
-      math::Jacobian analytical = skel->getWorldPositionJacobian(body);
-      math::Jacobian bruteForce
-          = skel->finiteDifferenceWorldPositionJacobian(body);
-
-      if (!equals(bruteForce, analytical, 1e-8))
+      for (int k = 0; k < 10; k++)
       {
-        std::cout << "World jac for skeleton \"" << skel->getName()
-                  << "\" body " << j << " " << std::endl
-                  << "Brute force jac col: " << std::endl
-                  << bruteForce << std::endl
-                  << "Analytical jac col: " << std::endl
-                  << analytical << std::endl
-                  << "Diff: " << std::endl
-                  << (bruteForce - analytical) << std::endl;
-        return false;
+        Eigen::Vector3s offset = Eigen::Vector3s::Random();
+        if (k == 0)
+          offset.setZero();
+        else if (0 < k && k <= 3)
+        {
+          offset = Eigen::Vector3s::Unit(k - 1);
+        }
+        math::Jacobian analytical
+            = skel->getWorldPositionJacobian(body, offset);
+        math::Jacobian bruteForce
+            = skel->finiteDifferenceWorldPositionJacobian(body, offset);
+
+        if (!equals(bruteForce, analytical, 1e-8))
+        {
+          std::cout << "World jac for skeleton \"" << skel->getName()
+                    << "\" body " << j << " " << std::endl
+                    << "Offset: " << std::endl
+                    << offset << std::endl
+                    << "Brute force jac col: " << std::endl
+                    << bruteForce << std::endl
+                    << "Analytical jac col: " << std::endl
+                    << analytical << std::endl
+                    << "Diff: " << std::endl
+                    << (bruteForce - analytical) << std::endl;
+          return false;
+        }
       }
     }
   }
@@ -7022,7 +7031,6 @@ public:
     setWorld(world);
   }
 };
-
 void renderWorld(WorldPtr world)
 {
   // Create a window for rendering the world and handling user input
