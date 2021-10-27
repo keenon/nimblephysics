f18dc78547266789fe711d51ef9283f18634f34b
Ericcsr <ericcsr@connect.hku.hk>
2021-10-27 14:13:58 +0000

    Prevent half cheetah realtime from blocking

--- unittests/comprehensive/test_HalfCheetahRealtime.cpp
+++ unittests/comprehensive/test_HalfCheetahRealtime.cpp
@@ -267,6 +267,141 @@ std::shared_ptr<LossFn> getSSIDVelLoss()
 
 #ifdef NO_VIS
 
+TEST(HALF_CHEETAH, FIRST_DEMO)
+{
+  std::shared_ptr<simulation::World> world = dart::utils::UniversalLoader::loadWorld(
+    "dart://sample/skel/half_cheetah.skel");
+  world->setPositions(Eigen::VectorXs::Zero(world->getNumDofs()));
+  world->setVelocities(Eigen::VectorXs::Zero(world->getNumDofs()));
+  std::cout <<"Time Step:" << world->getTimeStep() << std::endl;
+
+  std::shared_ptr<dynamics::Skeleton> halfCheetah
+      = world->getSkeleton("half_cheetah");
+  std::cout << "Springs: " << halfCheetah->getSpringStiffVector() << std::endl;
+  std::cout << "Damping: " << halfCheetah->getDampingCoeffVector() << std::endl;
+  
+  for(int i=0; i < 3; i++)
+  {
+    world->step();
+    std::cout << i << ":" <<std::endl;
+    std::cout << "Pos:" << std::endl << world->getPositions() << std::endl;
+    std::cout << "Vel:" << std::endl << world->getVelocities() << std::endl;
+    if(world->getPositions().hasNaN())
+    {
+      std::cout << "Got a NaN on timestep " << i << "!!" <<std::endl;
+      EXPECT_FALSE(world->getPositions().hasNaN());
+      break;
+    }
+  }
+}
+
+
+TEST(REALTIME, HALF_CHEETAH_PLOT)
+{
+  ////////////////////////////////////////////////////////////
+  // Create a cartpole example
+  ////////////////////////////////////////////////////////////
+
+  // World
+  std::shared_ptr<simulation::World> world = dart::utils::UniversalLoader::loadWorld(
+      "dart://sample/skel/half_cheetah.skel");
+  world->setPositions(Eigen::VectorXs::Zero(world->getNumDofs()));
+  world->setVelocities(Eigen::VectorXs::Zero(world->getNumDofs()));
+
+  
+  Eigen::VectorXs forceLimits
+    = Eigen::VectorXs::Ones(world->getNumDofs()) * 50;
+  forceLimits(0) = 0;
+  forceLimits(1) = 0;
+  world->setControlForceUpperLimits(forceLimits);
+  world->setControlForceLowerLimits(-1 * forceLimits);
+  world->setTimeStep(1.0 / 1000);
+  
+  size_t mass_idx = 10;
+
+  world->tuneMass(
+      world->getBodyNodeIndex(mass_idx),
+      WrtMassBodyNodeEntryType::INERTIA_MASS,
+      Eigen::VectorXs::Ones(1) * 5.0,
+      Eigen::VectorXs::Ones(1) * 0.2);
+
+  std::shared_ptr<LossFn> lossFn = getSSIDPosLoss();
+  //std::shared_ptr<LossFn> lossFn = getSSIDVelLoss();
+  //std::shared_ptr<LossFn> lossFn = getSSIDVelPosLoss();
+  ////////////////////////////////
+  // Set up a realtime world and controller
+  ////////////////////////////////////////////////////////////
+
+
+  int millisPerTimestep = world->getTimeStep() * 1000;
+  int steps = 5;
+  int inferenceHistoryMillis = steps * millisPerTimestep;
+
+  Eigen::VectorXs sensorDims = Eigen::VectorXs::Zero(2);
+  sensorDims(0) = world->getNumDofs();
+  sensorDims(1) = world->getNumDofs();
+  SSID ssid = SSID(world, lossFn, inferenceHistoryMillis, sensorDims,steps);
+  ssid.setInitialPosEstimator(
+      [](Eigen::MatrixXs sensors, long /* timestamp */) {
+        return sensors.col(0);
+      });
+  
+  ssid.setInitialVelEstimator(
+    [](Eigen::MatrixXs sensors, long)
+    {
+      return sensors.col(0);
+    }
+  );
+  
+  
+  world->setLinkMassIndex(2.0,mass_idx);
+  float init_mass = 1.0;
+  std::vector<Eigen::VectorXs> multi_loss;
+  
+  std::vector<s_t> solutions;
+  for (int i = 0; i < 200; i++)
+  {
+    long time = i * millisPerTimestep;
+    Eigen::VectorXs forces = Eigen::VectorXs::Ones(world->getNumDofs());
+    world->setControlForces(forces);
+    world->step();
+    ssid.registerControls(time, forces);
+    ssid.registerSensors(time, world->getPositions(),0);
+    ssid.registerSensors(time, world->getVelocities(),1);
+    
+    if(i%5==0 && i!=0)
+    {
+      multi_loss.push_back(ssid.runPlotting(time,5.0,0.2,200));
+      world->setLinkMassIndex(init_mass,mass_idx);
+      ssid.runInference(time);
+      init_mass = world->getLinkMassIndex(mass_idx);
+      std::cout << "Recovered mass after iteration "<<i<<": "
+                << init_mass << std::endl;
+      solutions.push_back(init_mass);
+      world->setLinkMassIndex(2.0,mass_idx);
+    }
+  }
+  Eigen::MatrixXs lossMatrix = Eigen::MatrixXs::Zero(200,multi_loss.size());
+  Eigen::VectorXs solutionVec = Eigen::VectorXs::Zero(solutions.size());
+  for(int i=0;i<multi_loss.size();i++)
+  {
+    lossMatrix.col(i) = multi_loss[i];
+    solutionVec(i) = solutions[i];
+  }
+  std::ofstream file;
+  file.open("/workspaces/nimblephysics/dart/realtime/saved_data/raw_data/Losses_half_cheetah.txt");
+  file<<lossMatrix.transpose();
+  file.close();
+  
+  std::ofstream sfile;
+  sfile.open("/workspaces/nimblephysics/dart/realtime/saved_data/raw_data/Solutions_half_cheetah.txt");
+  sfile<<solutionVec;
+  sfile.close();
+}
+#endif
+
+#ifdef ALL_TESTS
+
 TEST(HALF_CHEETAH, FULL_TEST)
 {
   // set precision to 256 bits (double has only 53 bits)
@@ -383,140 +518,7 @@ TEST(HALF_CHEETAH, FULL_TEST)
   {}
 }
 
-TEST(HALF_CHEETAH, FIRST_DEMO)
-{
-  std::shared_ptr<simulation::World> world = dart::utils::UniversalLoader::loadWorld(
-    "dart://sample/skel/half_cheetah.skel");
-  world->setPositions(Eigen::VectorXs::Zero(world->getNumDofs()));
-  world->setVelocities(Eigen::VectorXs::Zero(world->getNumDofs()));
-  std::cout <<"Time Step:" << world->getTimeStep() << std::endl;
 
-  std::shared_ptr<dynamics::Skeleton> halfCheetah
-      = world->getSkeleton("half_cheetah");
-  std::cout << "Springs: " << halfCheetah->getSpringStiffVector() << std::endl;
-  std::cout << "Damping: " << halfCheetah->getDampingCoeffVector() << std::endl;
-  
-  for(int i=0; i < 3; i++)
-  {
-    world->step();
-    std::cout << i << ":" <<std::endl;
-    std::cout << "Pos:" << std::endl << world->getPositions() << std::endl;
-    std::cout << "Vel:" << std::endl << world->getVelocities() << std::endl;
-    if(world->getPositions().hasNaN())
-    {
-      std::cout << "Got a NaN on timestep " << i << "!!" <<std::endl;
-      EXPECT_FALSE(world->getPositions().hasNaN());
-      break;
-    }
-  }
-}
-
-
-TEST(REALTIME, CARTPOLE_PLOT)
-{
-  ////////////////////////////////////////////////////////////
-  // Create a cartpole example
-  ////////////////////////////////////////////////////////////
-
-  // World
-  std::shared_ptr<simulation::World> world = dart::utils::UniversalLoader::loadWorld(
-      "dart://sample/skel/half_cheetah.skel");
-  world->setPositions(Eigen::VectorXs::Zero(world->getNumDofs()));
-  world->setVelocities(Eigen::VectorXs::Zero(world->getNumDofs()));
-
-  
-  Eigen::VectorXs forceLimits
-    = Eigen::VectorXs::Ones(world->getNumDofs()) * 50;
-  forceLimits(0) = 0;
-  forceLimits(1) = 0;
-  world->setControlForceUpperLimits(forceLimits);
-  world->setControlForceLowerLimits(-1 * forceLimits);
-  world->setTimeStep(1.0 / 1000);
-  
-  size_t mass_idx = 10;
-
-  world->tuneMass(
-      world->getBodyNodeIndex(mass_idx),
-      WrtMassBodyNodeEntryType::INERTIA_MASS,
-      Eigen::VectorXs::Ones(1) * 5.0,
-      Eigen::VectorXs::Ones(1) * 0.2);
-
-  std::shared_ptr<LossFn> lossFn = getSSIDPosLoss();
-  //std::shared_ptr<LossFn> lossFn = getSSIDVelLoss();
-  //std::shared_ptr<LossFn> lossFn = getSSIDVelPosLoss();
-  ////////////////////////////////
-  // Set up a realtime world and controller
-  ////////////////////////////////////////////////////////////
-
-
-  int millisPerTimestep = world->getTimeStep() * 1000;
-  int steps = 5;
-  int inferenceHistoryMillis = steps * millisPerTimestep;
-
-  Eigen::VectorXs sensorDims = Eigen::VectorXs::Zero(2);
-  sensorDims(0) = world->getNumDofs();
-  sensorDims(1) = world->getNumDofs();
-  SSID ssid = SSID(world, lossFn, inferenceHistoryMillis, sensorDims,steps);
-  ssid.setInitialPosEstimator(
-      [](Eigen::MatrixXs sensors, long /* timestamp */) {
-        return sensors.col(0);
-      });
-  
-  ssid.setInitialVelEstimator(
-    [](Eigen::MatrixXs sensors, long)
-    {
-      return sensors.col(0);
-    }
-  );
-  
-  
-  world->setLinkMassIndex(2.0,mass_idx);
-  float init_mass = 1.0;
-  std::vector<Eigen::VectorXs> multi_loss;
-  
-  std::vector<s_t> solutions;
-  for (int i = 0; i < 200; i++)
-  {
-    long time = i * millisPerTimestep;
-    Eigen::VectorXs forces = Eigen::VectorXs::Ones(world->getNumDofs());
-    world->setControlForces(forces);
-    world->step();
-    ssid.registerControls(time, forces);
-    ssid.registerSensors(time, world->getPositions(),0);
-    ssid.registerSensors(time, world->getVelocities(),1);
-    
-    if(i%5==0 && i!=0)
-    {
-      multi_loss.push_back(ssid.runPlotting(time,5.0,0.2,200));
-      world->setLinkMassIndex(init_mass,mass_idx);
-      ssid.runInference(time);
-      init_mass = world->getLinkMassIndex(mass_idx);
-      std::cout << "Recovered mass after iteration "<<i<<": "
-                << init_mass << std::endl;
-      solutions.push_back(init_mass);
-      world->setLinkMassIndex(2.0,mass_idx);
-    }
-  }
-  Eigen::MatrixXs lossMatrix = Eigen::MatrixXs::Zero(200,multi_loss.size());
-  Eigen::VectorXs solutionVec = Eigen::VectorXs::Zero(solutions.size());
-  for(int i=0;i<multi_loss.size();i++)
-  {
-    lossMatrix.col(i) = multi_loss[i];
-    solutionVec(i) = solutions[i];
-  }
-  std::ofstream file;
-  file.open("/workspaces/nimblephysics/saved_data/raw_data/Losses_half_cheetah.txt");
-  file<<lossMatrix.transpose();
-  file.close();
-  
-  std::ofstream sfile;
-  sfile.open("/workspaces/nimblephysics/saved_data/raw_data/Solutions_half_cheetah.txt");
-  sfile<<solutionVec;
-  sfile.close();
-}
-#endif
-
-#ifdef ALL_TESTS
 TEST(REALTIME, CARTPOLE_MPC)
 {
   ////////////////////////////////////////////////////////////
