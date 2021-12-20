65507f49538ceae0f6faf4ab0fad25c753670b87
Michelle Guo <shellguo95@gmail.com>
2021-12-20 00:39:00 -0800

    frictionless lcp in progress

--- python/new_examples/constraint_callback.py
+++ python/new_examples/constraint_callback.py
@@ -9,12 +9,29 @@ def dummy_callback(world: nimble.simulation.World):
 def lcp_callback(world: nimble.simulation.World):
     world.getConstraintSolver().lcpSolveCallback(world)
 
+def frictionless_lcp_callback(world: nimble.simulation.World):
+    # Backup and remove friction.
+    friction_coefs = []
+    bodies = []
+    for i in range(world.getNumBodyNodes()):
+        body = world.getBodyNodeIndex(i)
+        bodies.append(body)
+        friction_coefs.append(body.getFrictionCoef())
+        body.setFrictionCoef(0.0)
+        
+    # Frictionless LCP
+    lcp_callback(world)
+
+    # Restore friction.
+    for friction_coef, body in zip(friction_coefs, bodies):
+        body.setFrictionCoef(friction_coef)
+
 def main():
     world = nimble.loadWorld("../../data/skel/test/colliding_cube.skel")
     state = torch.tensor(world.getState())
     action = torch.zeros((world.getNumDofs()))
     solver = world.getConstraintSolver()
-    callbacks = [None, dummy_callback, lcp_callback]
+    callbacks = [None, dummy_callback, lcp_callback, frictionless_lcp_callback]
     for callback in callbacks:
         if callback is not None:
             solver.replaceSolveCallback(callback)
