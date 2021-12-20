1347d1a16870e41799612003fb9279d66515db2c
Michelle Guo <shellguo95@gmail.com>
2021-12-20 00:31:33 -0800

    multiple callbacks

--- python/new_examples/constraint_callback.py
+++ python/new_examples/constraint_callback.py
@@ -6,15 +6,20 @@ import torch
 def dummy_callback(world: nimble.simulation.World):
     pass
 
+def lcp_callback(world: nimble.simulation.World):
+    world.getConstraintSolver().lcpSolveCallback(world)
+
 def main():
     world = nimble.loadWorld("../../data/skel/test/colliding_cube.skel")
     state = torch.tensor(world.getState())
     action = torch.zeros((world.getNumDofs()))
     solver = world.getConstraintSolver()
-    solver.replaceSolveCallback(dummy_callback)
-    new_state = nimble.timestep(world, state, action)
-    print(state)
-    print(new_state)
+    callbacks = [None, dummy_callback, lcp_callback]
+    for callback in callbacks:
+        if callback is not None:
+            solver.replaceSolveCallback(callback)
+        new_state = nimble.timestep(world, state, action)
+        print(new_state)
 
 
 if __name__ == '__main__':
