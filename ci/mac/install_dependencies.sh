8aa5dbbf328b6a0a0d787389ac0de9a4dc5662a5
Nicholas Bianco <nbianco@stanford.edu>
2024-03-21 09:14:46 -0700

    Restore original workflow file (i.e., only build on release

--- ci/mac/install_dependencies.sh
+++ ci/mac/install_dependencies.sh
@@ -13,7 +13,6 @@ brew reinstall gcc
 export FC=$(which gfortran)
 echo "FC=$FC"
 
-# export MACOSX_DEPLOYMENT_TARGET="11.0"
 export MACOSX_DEPLOYMENT_TARGET="12.0"
 export CMAKE_FLAGS="-DCMAKE_OSX_ARCHITECTURES=x86_64"
 
