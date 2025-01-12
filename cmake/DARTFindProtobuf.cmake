76582a69bb48bbeafdef339740b3360acd1e11cb
Keenon Werling <keenonwerling@gmail.com>
2025-01-11 20:49:10 -0800

    Changing to any version of Protobuf

--- cmake/DARTFindProtobuf.cmake
+++ cmake/DARTFindProtobuf.cmake
@@ -2,11 +2,12 @@
 # All rights reserved.
 #
 # The list of contributors can be found at:
-#   https://github.com/dartsim/dart/blob/master/LICENSE
+# https://github.com/dartsim/dart/blob/master/LICENSE
 #
 # This file is provided under the "BSD-style" License
 
-find_package(Protobuf "3.14.0.0" EXACT REQUIRED)
+# find_package(Protobuf "5.29.2" EXACT REQUIRED)
+find_package(Protobuf REQUIRED)
 
 message(STATUS "Protobuf_LIBRARIES: ${Protobuf_LIBRARIES}")
 
@@ -17,6 +18,7 @@ if(Protobuf_FOUND AND NOT TARGET protobuf::libprotobuf)
     INTERFACE_LINK_LIBRARIES "${Protobuf_LIBRARIES}"
   )
 endif()
+
 if(Protobuf_FOUND AND NOT TARGET protobuf::libprotoc)
   add_library(protobuf::libprotoc INTERFACE IMPORTED)
   set_target_properties(protobuf::libprotoc PROPERTIES
@@ -24,6 +26,7 @@ if(Protobuf_FOUND AND NOT TARGET protobuf::libprotoc)
     INTERFACE_LINK_LIBRARIES "${Protobuf_PROTOC_LIBRARIES}"
   )
 endif()
+
 if(Protobuf_FOUND AND NOT TARGET protobuf::libprotobuf-lite)
   add_library(protobuf::libprotobuf-lite INTERFACE IMPORTED)
   set_target_properties(protobuf::libprotobuf-lite PROPERTIES
